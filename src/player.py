0df70b3cdd743342447827dee23ea83ba74a49e7
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2019-05-01 15:13:49 -0400

    Added Player class

--- src/player.py
+++ src/player.py
@@ -1,3 +1,8 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
-#
+
+class Player:
+    def _init_(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
+
